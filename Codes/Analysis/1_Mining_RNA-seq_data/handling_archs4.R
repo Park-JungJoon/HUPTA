ff475534d5d52b3cf86eea61b40206b7e5f5e525
ParkJungJoon <97942772+Park-JungJoon@users.noreply.github.com>
2024-05-08 07:09:40 +0900

    Create handling_archs4.R

---
+++ Codes/Analysis/1_Mining_RNA-seq_data/handling_archs4.R
@@ -0,0 +1,35 @@
+BiocManager::install("rhdf5")
+library("rhdf5")    # can be installed using Bioconductor
+destination_file = "the path to the file" # the path to the file, h5 file from archs4
+extracted_expression_file = "file_name.tsv" # gene count table's file name 
+
+setwd("")
+
+# Build meta data of GEO 
+samples = h5read(destination_file, "meta/samples/geo_accession")
+tissues = h5read(destination_file, "meta/sample/source_name_ch1")
+scp = h5read(destination_file, "meta/sample/singlecellprobability")
+
+meta_sample = cbind(samples,tissues,scp)
+write.table(meta_sample, file = 'sample_meta_data.tsv', row.names = FALSE,quote = FALSE,sep='\t')
+
+# Build Gene information table
+genes = h5read(destination_file, "meta/genes/gene_symbol")
+ensg =  h5read(destination_file, "meta/genes/ensembl_gene_id")
+gene_symbol = h5read(destination_file, "/meta/genes/gene_symbol")
+
+gene_data = cbind(genes,ensg,gene_symbol)
+write.table(gene_data, file = 'gene_data.txt', row.names = FALSE,quote = FALSE,sep='\t')
+
+## Expression table
+# Identify columns to be extracted
+sample_locations = which(samples %in% samp)
+# extract gene expression from compressed data
+expression = t(h5read(destination_file, "data/expression", index=list(sample_locations, 1:length(genes))))
+H5close()
+rownames(expression) = genes
+colnames(expression) = samples[sample_locations]
+
+# Print file
+write.table(expression, file=extracted_expression_file, sep="\t", quote=FALSE, col.names=NA)
+print(paste0("Expression file was created at ", getwd(), "/", extracted_expression_file))
