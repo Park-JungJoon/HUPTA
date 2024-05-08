7b6f6a8bbaacf02ede871c236ebbd5009d4da8e6
ParkJungJoon <97942772+Park-JungJoon@users.noreply.github.com>
2024-05-08 21:56:36 +0900

    Update cv_calculate.R

--- Codes/Analysis/3_Housekeeping_gene/cv_calculate.R
+++ Codes/Analysis/3_Housekeeping_gene/cv_calculate.R
@@ -1,5 +1,5 @@
 library('data.table')
-cal_cv <- function(input,output){
+cal_cv <- function(input,output,exp_output){
     # Load data; We used fread due to the large size of our data, but you can also use read.table.
     file = fread(input, sep = '\t', header = TRUE)
     # Gene should be located at column. If there are samples in columns, use t() function in R 
@@ -13,4 +13,6 @@ cal_cv <- function(input,output){
 geo_gct = "directory of geo gene count table"
 geo_output = "directory of geo cv table"
 gtex_gct = "directory of gtex gene count table"
+gtex_output = "directory of gtex cv table"
 tcga_gct = "directory of tcga gene count table"
+tcga_output = "directory of tcga cv table"
