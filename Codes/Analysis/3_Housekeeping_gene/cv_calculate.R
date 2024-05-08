f912f969d311e440c576a804a0e4f16006ddf751
ParkJungJoon <97942772+Park-JungJoon@users.noreply.github.com>
2024-05-08 20:34:56 +0900

    Create cv_calculate.R

---
+++ Codes/Analysis/3_Housekeeping_gene/cv_calculate.R
@@ -0,0 +1,16 @@
+library('data.table')
+cal_cv <- function(input,output){
+    # Load data; We used fread due to the large size of our data, but you can also use read.table.
+    file = fread(input, sep = '\t', header = TRUE)
+    # Gene should be located at column. If there are samples in columns, use t() function in R 
+    gene = colnames(df.file)
+    # Calculate coefficient of variants
+    cv <- sapply(df.file,function(x) mean(x))
+    cv_df <- merge(gene,cv)
+    write.table(cv_df, file = output, quote = FALSE, sep = '\t')
+}
+
+geo_gct = "directory of geo gene count table"
+geo_output = "directory of geo cv table"
+gtex_gct = "directory of gtex gene count table"
+tcga_gct = "directory of tcga gene count table"
